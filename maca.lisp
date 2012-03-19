c0006f288e8fd7c92bb6c74434e76e86bde74b39
guicho <guicho@guicho-desktop.(none)>
2012-03-20 03:58:08 +0900

    improved more

--- maca.lisp
+++ maca.lisp
@@ -1,5 +1,6 @@
 (ql:quickload :alexandria)
 (ql:quickload :cl-match)
+(ql:quickload :anaphora)
 
 (defpackage maca
   (:use :common-lisp :cl-user :alexandria :cl-match :anaphora))
@@ -19,8 +20,21 @@
     import int interface long native package private protected
     public short static super synchronized throws transient volatile))
 
-(defparameter 
 
+(defparameter *constants*
+  '((t . "true")
+    (true . "true")
+    (on . "true")
+    (yes . "true")
+    (false . "false")
+    (off . "false")
+    (no . "false")
+    (undefined . "void 0")
+    (null . "void 0")
+    (this . "this")
+    (@ . "this")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; utility
 
 (defun not-uniquep (lst &key (test #'eq))
@@ -36,17 +50,59 @@
 (defun uniquep (lst &key (test #'eq))
   (not (not-uniquep lst :test test)))
 
-;; writer
-
 (defun prints (&rest args)
   (format nil "~{~a~}" args))
 
+
 (defmacro defw (name args &rest contents)
   `(defun ,name ,args
-       (prints ,@contents)))
+     (prints ,@contents)))
 
-(defw m-var (var val)
-  "var " var "=" val)
+;; (defmacro defw (name args &rest contents)
+;;   (let ((args (append args (unless (member '&key args) '(&key)) '(must-return-value))))
+;;     `(defun ,name ,args
+;;        (prints ,@contents))))
+
+(defun gensym-js (&optional (thing "G"))
+  (symbol-name (gensym thing)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; rewriter
+
+;; helper
+
+(defw m-paren (arg) "(" (m-compile arg) ")")
+(defun m-comma (&rest args)
+  (format nil "~{~a~^,~}" (mapcar #'m-compile args)))
+
+
+;; -----------------------------
+;; sentences
+
+(defun m-sentences (sents)
+  (format nil "~{~a;~%~}" (mapcar #'m-compile sents)))
+
+;; -----------------------------
+;; keywords
+
+(defun m-var (var &optional val)
+  (if val
+      (prints "var " var "=" (m-compile val))
+      (prints "var " var)))
+
+;; (defw m-var (var val)
+;;   (if must-return-value
+;;       (prints "(var " (m-compile var) "=" (m-compile val) ")")
+;;       (prints "var " (m-compile var) "=" (m-compile val))))
+
+
+(defw m-return (&optional value)
+  "return " (m-compile value))
+
+;; -----------------------------
+;; function and function calls
+(defun m-function-call (op args)
+  (format nil "~a(~{~a~^,~})" (symbol-name op) (mapcar #'m-compile args)))
 
 (defun m-args (&rest args)
   (let ((args (flatten args)))
@@ -54,42 +110,246 @@
 	  ((some #'(lambda (a) (not (symbolp a))) args) (error (prints "invalid argument in the list")))
 	  (t (format nil "~{~a~^,~}" args)))))
 
-(defw m-fn (args body)
-  "function(" (m-args args) "){\n"
-  (mapcar #'m-sentences body)
-  "}\n")
+(defw m-function (args body)
+  "function(" (m-args args) "){"
+  (m-sentences (butlast body))
+  (m-return (car (last body)))
+  "}")
 
-(defw m-sentence (sent)
-  (m-compile sent) ";\n")
+;; not implemented
+(defw m-inherit-this-function (args body)
+  "function(" (m-args args) "){"
+  (m-sentences (butlast body))
+  (m-return (car (last body)))
+  "}")
 
-(defw m-sentences (sents)
-  (reduce #'(lambda (prev now) (concatenate 'string prev now))
-	  (mapcar #'m-sentence sents)))
+(defw m-procedure-function (args body)
+  "function(" (m-args args) "){"
+  (m-sentences (butlast body))
+  (m-return (car (last body)))
+  "}")
 
+(defw m-inline-function (args body)
+  "function(" (m-args args) "){"
+  (m-sentences (butlast body))
+  (m-return (car (last body)))
+  "}")
+
+;; -----------------------------
+;; iteration and conditional expression
+
+(defw m-? (condition then &optional (else 'undefined))
+  (m-compile condition) "?" (m-compile then) ":" (m-compile else))
+
+(defun m-if (condition then &optional else)
+  (if else
+      (format nil "if(~a){~%~a}else{~%~a}~%" (m-compile condition) (m-compile then) (m-compile else))
+      (format nil "if(~a){~%~a}~%" (m-compile condition) (m-compile then))))
+
+;; (defw m-iter-array (val array body &optional (key (gensym-js)))
+;;   (let ((len (gensym-js "l"))
+;; 	(ref (gensym-js "ref")))
+;;     `(maca (var ,key 0)
+;; 	   (var ,len (,array > length))
+;; 	   (var ,ref))
+;;     "for(;" key "<len;" key "++){"
+;;     `(maca (= ,ref (,array > ,key)))
+;;     (m-compile body)
+;;     "}"))
+
+;; -----------------------------
+;; math and operators
+
+(defparameter *assignments*
+  '(= += -= *= /= <<= >>= >>>= &= ^= ))	;|= 
+(defw m-assignments (op to from)
+  (m-compile to) " " op " " (m-compile from))
+
+(defparameter *infixes* 
+  '(+ - * / % << >> >>> &&)) 		;||
+(defun m-infix (op vars)
+  (aif (cdr vars)
+       (prints "(" (m-compile (car vars)) " " op " " (m-infix op it) ")")
+       (m-compile (car vars))))
+
+(defparameter *comparisons* 
+  '(== != === !== > < >= <=))
+
+(defw m-comparison-primitive (op var1 var2)
+  "(" (m-compile var1) " " op " " (m-compile var2) ")")
+
+(defun m-comparison (op vars)
+  (if (third vars)
+      (prints (m-comparison-primitive op (first vars) (second vars))
+	      " && "
+	      (m-comparison op (cdr vars)))
+      (m-comparison-primitive op (first vars) (second vars))))
+
+(defparameter *mono-ops*
+  '(++ -- ^ ~ ! 
+    new set get typeof instanceof void delete var))
+(defw m-mono-ops (op val)
+  op " (" (m-compile val) ")")
+
+;; -----------------------------
+;; array and object literals
 (defun m-obj (key-value-lst)
   (format nil "{~%~{~a:~a~^,~%~}~%}"
 	  (mapcar #'(lambda (val) (if (keywordp val) val (m-compile val)))
 		  key-value-lst)))
 
-(defmacro maca (&body body)
-  `(m-compile ',body))
+(defw m-accessor (obj accessor)
+  (m-compile obj) "." (if (cdr accessor) 
+			  (m-compile accessor)
+			  (m-compile (car accessor))))
 
-(maca
-  (= number 42)
-  (= opposite true))
+(defmacro print-with-environtment (envname &body body)
+  `(prints (format nil "~{~a~}" 
+		   (m-compile 
+		    (mapcar #'(lambda (varname) `(var ,varname))
+			    ,envname)))
+	   ,@body))
 
-(defun m-compile (lst)
+(defun m-exist-accessor (obj accessor &key env)
+  (let ((ref (gensym-js))
+	(child (car accessor)))
+    (setf (car accessor) ref)
+    (if env (push ref env))
+    (prints "("
+	    (m-? `(!= (paren (= ,ref (,obj > ,child))) null)
+		 (if (cdr accessor)
+		     accessor
+		     ref))
+	    ")")))
+
+(defw m-prototype-accessor (obj accessor)
+  (m-compile obj) ".prototype." (if (cdr accessor) 
+			  (m-compile accessor)
+			  (m-compile (car accessor))))
+
+;; -----------------------------
+;; main compilation
+
+(defmacro rewrite (name &rest args)
+  `(values (,name ,@args) ',name))
+
+;; todo:
+;;   add an enviromental valiable
+;;   add "must-return-value" option
+(defun m-compile (lst) 		;&optional (value nil)
   (match lst
+    ((when (assoc val *constants*) (type atom val)) (cdr (assoc val *constants*)))
     ((type atom val) (values val 'atom))
-    ((list (or '= 'set) var val) (values (m-var (m-compile var) (m-compile val)) 'var))
-    ;; ((list* '-> (list* args) body) (values (m-fn args body) 'fn))
-    ;; ((list* 'for (type atom val) 'in array)
-    ;;  (values (iter-array val key array) 'iter-array))
-    ;; ((list* 'for (type atom val) (type atom key) (when (not eq key 'in) 'in array))
-    ;;  (values (iter-array val key array) 'iter-array-key))
-    ;; ((list* (type atom obj) '> accessor) (values (prints obj "." (m-compile accessor)) 'accessor))
-    ;; ((list* (type atom obj) '? accessor) (values (prints obj "?." (m-compile accessor)) 'exist-accessor))
-    ;; ((list* (type atom obj) '-> accessor) (values (prints obj ".prototype." (m-compile accessor)) 'prototype))
-    ;; ((list* (type keyword key) rest) (values (m-obj lst) 'obj-literal))
-    ((list* sentences) (values (m-sentences sentences) 'sentences))
+    ;; parenthesis operator: should avoid using it, its just meant to be used by the compiler
+    ((list 'paren clause)                                (rewrite m-paren clause))
+    ((list 'var (or (type symbol v1)
+		    (type string v1)
+		    (type keyword v1)))  (rewrite m-var v1))
+    ((list 'var (or (type symbol v1)
+		    (type string v1)
+		    (type keyword v1)) v2)  (rewrite m-var v1 v2))
+    ((list* 'var _ rest)              (error "invalid variable name"))
+    ((when (member op *assignments*) (list op v1 v2))  (rewrite m-assignments op v1 v2))
+    ((when (member op *infixes*)     (list* op vars))  (rewrite m-infix op vars))
+    ((when (member op *mono-ops*)    (list op var))    (rewrite m-mono-ops op var))
+    ((when (member op *comparisons*) (list* op vars))  (rewrite m-comparison op vars))
+    ((list (as op 'in) v1 v2)                          (rewrite m-comparison-primitive op v1 v2))
+    ((list '? cond then else)                          (rewrite m-? cond then else))
+    ;; comma operator: should avoid using it, its just meant to be used by the compiler
+    ((list* 'comma clauses)                                (rewrite m-comma clauses))
+    ;; ((list 'set var val)                                   (rewrite m-set var val))
+    ((list* '-> (list* args) body)                         (rewrite m-function args body))
+    ((list* '=> (list* args) body)                         (rewrite m-inherit-this-function args body))
+    ((list* '-/> (list* args) body)                        (rewrite m-procedure-function args body))
+    ((list* '-/  (list* args) body)                        (rewrite m-inline-function args body))
+    ;; ((list* 'for val 'in array body)                       (rewrite m-iter-array val nil array))
+    ;; ((list* 'for val key 'in array body)                   (rewrite m-iter-array val key array))
+    ;; ((list* 'for val 'of array)                            (rewrite m-iter-obj val nil array))
+    ;; ((list* 'for val key 'of array)                        (rewrite m-iter-obj val key array))
+    ;; ((list* 'for 'own val key 'of array)                   (rewrite m-iter-obj val key array :own t))
+    ((list 'if cond then)                                  (rewrite m-if cond then))
+    ((list 'if cond then else)                             (rewrite m-if cond then else))
+    ;; ((list 'try body 'catch (list var) error)              (rewrite m-try body var error nil))
+    ;; ((list 'try body 'catch (list var) error 'finally fin) (rewrite m-try body var error fin))
+    ;; ((list* 'try body _)                                   (error "invalid try-catch statement"))
+    ((list* obj '> accessor)                               (rewrite m-accessor obj accessor))
+    ((list* obj '? accessor)                               (rewrite m-exist-accessor obj accessor))
+    ((list* obj '-> accessor)                              (rewrite m-prototype-accessor obj accessor))
+    ((list* (type keyword key) rest)                       (rewrite m-obj lst))
+    ((list* (type atom op) arguments)                      (rewrite m-function-call op arguments))
+    ((list* sentences)                                     (rewrite m-sentences sentences))
     ))
+
+
+(defmacro maca (&body body)
+  (if (= (length body) 1)
+      `(m-compile ',@body)
+      `(m-compile ',body)))
+
+;; atom
+(maca a)
+
+;; constants
+(maca undefined)
+
+;; assignments
+(maca (= number 3))
+(maca (var number 3))
+(maca (var (+ 2 1) 3))
+
+;; infix
+(maca (>>> number 3))
+(maca (+ a 3))
+(maca (+ a 3 (>>> number 3)))
+
+;; comparison
+(maca (< number 50))
+(maca (== 5 3))
+(maca (== 5 3 4))
+(maca (>= 5 3 4))
+
+;; mono-ops
+(maca (new (number)))
+(maca (typeof 5))
+
+;; in
+(maca (in 5 array))
+
+;; function definition
+(maca (-> (a b c)
+	  (= d (- a b c))
+	  (+ a b c d)))
+
+;; conditional expression
+(maca (if (a b c)
+	  (= d (- a b c))))		;one-line
+(maca (if (a b c)
+	  (= d (- a b c))
+	  (= d (- a b c))))		;one-line else
+(maca (if (a b c)
+	  ((= d (- a b c))
+	   (= d (- a b c)))
+	  (= e (+ a b c d))))		;multi-line then
+
+(maca (if (a b c)			;multi-line both 
+	  ((= d (- a b c))
+	   (= e (+ a b c d)))
+	  ((= e (+ a b c d))
+	   (= d (- a b c)))))
+
+(maca (? a b c))
+
+;; object accessor
+(maca (obj > attibute))
+(maca (obj > child > grandchild))
+(maca (granpa > parent > obj > child > grandchild))
+(maca (obj > (child) > (grandchild)))
+(maca (obj > (child) > grandchild))
+(maca (obj -> child))
+(maca (obj -> (method)))
+
+(maca (a ? b))
+(maca (a ? b ? c))
+
+(maca (a > b ? c ? d > (e) > (f) ?  g -> h))
+
