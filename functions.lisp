325b09b9651ba106999a7b8fac8348e5890f07ba
guicho <guicho@guicho-desktop.(none)>
2012-05-03 13:53:32 +0900

    separated the implementation of m-primitive-function from that of m-procedure-function. m-procedure-function now recongizes &rest and &key arguments.

--- functions.lisp
+++ functions.lisp
@@ -7,24 +7,24 @@
   '(((list* 'global sentences)          (rewrite m-global sentences))
 	((list* 'function name (as type (or '-> '-/>)) (list* args) body)
 	 (rewrite m-function-declaration name type args body))
+	((list* '->prim (list* args) body)      (rewrite m-primitive-function args body))
 	((list* '-> (list* args) body)      (rewrite m-function args body))
 	((list* '=> (list* args) body)      (rewrite m-inherit-this-function args body))
 	((list* '-/> (list* args) body)     (rewrite m-procedure-function args body))
 	((list* '=/> (list* args) body)     (rewrite m-inherit-this-procedure-function args body))
 	((list* '-/ name (list* args) body) (rewrite m-inline-function name args body))))
 
-(defmacro check-args (args &body body)
-  `(let ((args (flatten ,args)))
-     (cond ((not-uniquep args)        (error "duplicated args: ~a" (not-unique args)))
-		   ((notevery #'symbolp args) (error "invalid args:~a" args))
-		   (t ,@body))))
+(defun check-args (args)
+  (let ((args (flatten args)))
+	(cond ((not-uniquep args)        (error "duplicated args: ~a" (not-unique args)))
+		  ((notevery #'symbolp args) (error "invalid args:~a" args)))))
 
 ;; ----------------------------------------------------------------
 ;; m-global and m-function, m-procedure-function has the similar definitions
 ;; 
 
 (defmaca (m-global :environment env :is-value t) (body)
-  (compile-let* env
+  (compile-let* env 					;push the variables
 	  ((compiled-body body))
 	(declare (ignore compiled-body))
 	`(sentences ,@(aif +variables+
@@ -32,32 +32,43 @@
 				,@(nreverse +initializations+)
 				,@body)))
 
-;; (defmaca (m-global :environment env :is-value t) (body)
-;;   (compile-let* env 
-;; 	  ((compiled-body `(sentences ,@body))
-;; 	   (compiled-header 
-;; 		(let ((vars (aif +variables+
-;; 						 `(glue var space (comma ,@(uniquify it)))))
-;; 			  (init (nreverse +initializations+)))
-;; 		  (if (or vars init)
-;; 			  `(sentences ,vars	,@init)))))
-;; 	`(glue ,compiled-header ,compiled-body)))
-
 ;; the most primitive javascript function
-(defmaca (m-procedure-function :environment env :is-value t) (args body)
-  (check-args args
-	(compile-let* env
-		((func-header `(glue function (paren (comma ,@args)))))
-	  (let* ((cl (make-closure :arguments args))
-			 (new-env (cons cl env)))
-		;; search/push the variables 
-		(compile-let* new-env ((compiled-body body))
-		  (declare (ignore compiled-body))
-		  `(glue ,func-header
-				 (blk (,@(aif (closure-variables cl)
-							  `((glue var space (comma ,@(uniquify it)))))
+(defmaca (m-primitive-function :environment env :is-value t) (args body)
+  (check-args args)
+  (let ((cl (make-closure :arguments args)))
+	  ;; search/push the variables 
+	  (compile-let* (cons cl env) 
+		  ((func-header `(glue function (paren (comma ,@args))))
+		   (compiled-body body))
+		(declare (ignore compiled-body))
+		`(glue ,func-header
+			   (blk (,@(aif (closure-variables cl)
+							`((glue var space (comma ,@(uniquify it)))))
 					   ,@(nreverse (closure-initializations cl))
-					   ,@body))))))))
+					   ,@body))))))
+
+(defun rmember (list object)
+  (nreverse (cdr (member object (reverse list)))))
+
+(defmaca (m-procedure-function :is-value t) (args body)
+  (let ((rest (member '&rest args))
+		(keys (member '&key args))
+		(butrest (rmember args '&key))
+		(butkeys (rmember args '&rest)))
+	(when (and rest keys)
+	  (error "you can't use &key and &rest at the same time : ~a" args))
+	(when (cddr rest)
+	  (error "superfluous arguments after the &rest argument: ~a" rest))
+	(let* ((regular-args (or butrest butkeys args))
+		   (len (length regular-args)))
+	  `(->prim ,regular-args
+			   ,(if rest `(var ,(cadr rest) (-array >> slice > (call arguments ,len))))
+			   ,@(if keys
+					 (with-gensyms (keyword-obj)
+					   `((var ,keyword-obj (arguments > ,len))
+						 ,@(loop for key-name in keys collect
+							 `(var ,key-name (,keyword-obj > ,key-name))))))
+			   ,@body))))
 
 (defmaca (m-function :is-value t) (args body)
   `(-/> ,args
